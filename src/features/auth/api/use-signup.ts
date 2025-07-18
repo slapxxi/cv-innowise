4157ebf47c1f9a4ef9ba6f2e4c7727add2d9cbbd
Daniil <daniil.shishaev@gmail.com>
2025-07-18 18:25:52 +0300

    feat: signup query

--- src/features/auth/api/use-signup.ts
+++ src/features/auth/api/use-signup.ts
@@ -3,34 +3,31 @@ import type { AuthInput, AuthResult } from 'cv-graphql';
 import { graphQLClient } from '~/shared/api/graphql.ts';
 import { useNavigate } from '@tanstack/react-router';
 
-const LOGIN_QUERY = `
-  query Login($auth: AuthInput!) {
-    login(auth: $auth) {
-      access_token
+const SIGNUP_QUERY = `
+  mutation Signup($auth:AuthInput!){
+    signup(auth: $auth) {
       user {
         id
         email
       }
+      access_token
     }
   }
 `;
 
-export const useLoginMutation = () => {
+export const useSignupMutation = () => {
   const navigate = useNavigate();
   return useMutation<AuthResult, Error, AuthInput>({
     mutationFn: async (auth: AuthInput) => {
-      const response = await graphQLClient.request<{ login: AuthResult }>(LOGIN_QUERY, { auth });
+      const response = await graphQLClient.request<{ signup: AuthResult }>(SIGNUP_QUERY, { auth });
       console.log(response);
-      return response.login;
+      return response.signup;
     },
     onSuccess: (response) => {
       if (response.access_token) {
         localStorage.setItem('access_token', response.access_token);
       }
-      navigate({ to: '/' });
-    },
-    onError: (error: Error) => {
-      console.log(error.reponse.errors[0].message);
+      navigate({ to: '/auth/login' });
     },
   });
 };
--- src/routes/auth/_authLayout.signup.route.tsx
+++ src/routes/auth/_authLayout.signup.route.tsx
@@ -1,6 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { useTranslation } from 'react-i18next';
 import { Button, PasswordField, TextField, Title } from '~/shared';
+import { useForm } from 'react-hook-form';
+import type { AuthInput } from 'cv-graphql';
+import { useSignupMutation } from '~/features/auth/api/use-signup.ts';
 
 export const Route = createFileRoute('/auth/_authLayout/signup')({
   component: RouteComponent,
@@ -8,9 +11,15 @@ export const Route = createFileRoute('/auth/_authLayout/signup')({
 
 function RouteComponent() {
   const { t } = useTranslation();
+  const nav = Route.useNavigate();
+  const { register, handleSubmit } = useForm<AuthInput>();
+  const { mutate: signupMutation, isPending } = useSignupMutation();
 
+  const onSubmit = (data: AuthInput) => {
+    signupMutation(data);
+  };
   return (
-    <section className="flex flex-col">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
       <header className="flex flex-col items-center gap-6 mb-10">
         <Title asChild>
           <h2>{t('Register')}</h2>
@@ -19,14 +28,18 @@ function RouteComponent() {
       </header>
 
       <div className="flex flex-col gap-4">
-        <TextField label={t('Email')} type="email" />
-        <PasswordField label={t('Password')} />
+        <TextField label={t('Email')} type="email" {...register('email')} />
+        <PasswordField label={t('Password')} {...register('password')} />
       </div>
 
       <div className="mt-15 flex flex-col gap-2 self-center">
-        <Button>{t('Signup')}</Button>
-        <Button variant="text">{t('I have an account')}</Button>
+        <Button type="submit" disabled={isPending}>
+          {t('Signup')}
+        </Button>
+        <Button variant="text" onClick={() => nav({ to: '/auth/login' })}>
+          {t('I have an account')}
+        </Button>
       </div>
-    </section>
+    </form>
   );
 }
