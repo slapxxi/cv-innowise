0c9ea2efbc700cd11e3d1aec2bb2e95c20e1f742
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-07-19 15:41:06 +0300

    Dark mode for users layout

--- src/routes/_mainLayout/route.tsx
+++ src/routes/_mainLayout/route.tsx
@@ -25,9 +25,10 @@ function RouteComponent() {
                   activeOptions={{ exact: true }}
                   to={to}
                   activeProps={{
-                    className: 'text-neutral-800 bg-neutral-200 hover:bg-neutral-200 hover:text-neutral-800',
+                    className:
+                      'text-neutral-800 bg-neutral-200 hover:bg-neutral-200 hover:text-neutral-800 dark:bg-neutral-600 dark:text-white dark:hover:bg-neutral-600 dark:hover:text-white',
                   }}
-                  className="flex items-center gap-4 rounded-full px-6 py-3 text-base text-neutral-600 hover:bg-neutral-200 hover:text-neutral-700"
+                  className="flex items-center gap-4 rounded-full px-6 py-3 text-base text-neutral-600 hover:bg-neutral-200 hover:text-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-600 dark:hover:text-white"
                 >
                   <Icon />
                   {t(name)}
@@ -39,7 +40,8 @@ function RouteComponent() {
                 activeOptions={{ exact: true }}
                 to="/cvs"
                 activeProps={{
-                  className: 'text-neutral-800 bg-neutral-200 hover:bg-neutral-200 hover:text-neutral-800',
+                  className:
+                    'text-neutral-800 bg-neutral-200 hover:bg-neutral-200 hover:text-neutral-800 dark:bg-neutral-600 dark:text-white',
                 }}
                 className="hidden items-center gap-4 rounded-full px-6 py-3 text-base text-neutral-600 hover:bg-neutral-200 hover:text-neutral-700 md:flex"
               >
@@ -51,9 +53,9 @@ function RouteComponent() {
               <Link
                 to="/users/$userId/profile"
                 params={{ userId: '1' }}
-                className="flex items-center gap-4 rounded-full p-1 pr-6 text-base hover:bg-neutral-200"
+                className="flex items-center gap-4 rounded-full p-1 pr-6 text-base hover:bg-neutral-200 dark:text-neutral-300 dark:hover:bg-neutral-600 dark:hover:text-white"
               >
-                <UserAvatar className="size-10 bg-primary text-white">H</UserAvatar>
+                <UserAvatar className="size-10 bg-primary text-white dark:text-neutral-600">H</UserAvatar>
                 <span className="overflow-hidden text-nowrap text-ellipsis">Rostislav Bobrov</span>
               </Link>
             </li>
