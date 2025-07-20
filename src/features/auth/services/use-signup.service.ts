f66845d0ea1f77710fc70f4e75e1b6281198f3d1
Daniil <daniil.shishaev@gmail.com>
2025-07-20 15:19:26 +0300

    fix: small edits

--- src/features/auth/services/use-signup.service.ts
+++ src/features/auth/services/use-signup.service.ts
@@ -1,11 +1,10 @@
 import { useMutation, type UseMutationOptions } from '@tanstack/react-query';
 import { signup, type SignupData, type SignupError, type SignupParams } from '~/shared';
-import type { AuthInput } from 'cv-graphql';
 
 export function useSignup(params: UseMutationOptions<SignupData, SignupError, SignupParams>) {
   const { mutate, ...mutation } = useMutation<SignupData, SignupError, SignupParams>({
     ...params,
-    mutationFn: async (params: AuthInput) => {
+    mutationFn: async (params: SignupParams) => {
       const signupResult = await signup(params);
 
       if (signupResult.ok) {
--- src/routes/auth/_authLayout.login.route.tsx
+++ src/routes/auth/_authLayout.login.route.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router';
 import i18n from '~/app/i18n.ts';
 import { LoginForm } from '~/features';
+import { Title } from '~/shared';
+import { t } from 'i18next';
 
 export const Route = createFileRoute('/auth/_authLayout/login')({
   head: () => ({ meta: [{ title: i18n.t('Login') }] }),
@@ -8,5 +10,15 @@ export const Route = createFileRoute('/auth/_authLayout/login')({
 });
 
 function RouteComponent() {
-  return <LoginForm />;
+  return (
+    <section className="flex flex-col">
+      <header className="flex flex-col items-center gap-6 mb-10">
+        <Title asChild>
+          <h2>{t('Welcome back')}</h2>
+        </Title>
+        <p>{t('Happy to see you')}</p>
+      </header>
+      <LoginForm />
+    </section>
+  );
 }
