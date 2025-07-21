01879784bb0e771cb8fc850e8cdc5fb461a1ac30
Daniil <daniil.shishaev@gmail.com>
2025-07-21 18:09:30 +0300

    feat: token refreshing client "getAuthClient"

--- src/pages/index/index.page.tsx
+++ src/pages/index/index.page.tsx
@@ -1,11 +1,9 @@
 import { Link } from '@tanstack/react-router';
 import Button from '@mui/material/Button';
 import { useTranslation } from 'react-i18next';
-import { useAutoRefetchToken } from '~/shared';
 
 export function IndexPage() {
   const { t } = useTranslation();
-  useAutoRefetchToken();
   return (
     <div className="p-2 flex flex-col gap-4">
       <Link to="/about">About</Link>
--- src/shared/lib/http/graphql.http.ts
+++ src/shared/lib/http/graphql.http.ts
@@ -1,11 +1,43 @@
+import type { UpdateTokenResult } from 'cv-graphql';
 import { GraphQLClient } from 'graphql-request';
+import { getCookie, setCookie } from 'typescript-cookie';
+
 export { ClientError, gql } from 'graphql-request';
 
-// todo: move to env
-const API_URL = 'https://cv-project-js.inno.ws/api/graphql';
+const API_URL = import.meta.env.VITE_API_URL;
 
-export const graphQLClient = new GraphQLClient(API_URL, {
-  headers: () => ({
-    Authorization: localStorage.getItem('access_token') ? `Bearer ${localStorage.getItem('access_token')}` : '',
-  }),
+export const unAuthClient = new GraphQLClient(API_URL, {
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
+const REFRESH_TOKEN_MUTATION = `
+  mutation {
+    updateToken {
+      access_token
+      refresh_token
+    }
+  }
+`;
+
+export async function refreshAccessToken(): Promise<string> {
+  const refreshToken = getCookie('refresh_token');
+  if (!refreshToken) throw new Error('No refresh token');
+  const graphQLClient = new GraphQLClient(API_URL, {
+    headers: {
+      Authorization: refreshToken ? `Bearer ${refreshToken}` : '',
+    },
+  });
+  const { updateToken } = await graphQLClient.request<{ updateToken: UpdateTokenResult }>(REFRESH_TOKEN_MUTATION);
+
+  localStorage.setItem('access_token', updateToken.access_token);
+
+  setCookie('refresh_token', updateToken.refresh_token, {
+    sameSite: 'strict',
+    secure: true,
+    path: '/',
+    expires: 7,
+  });
+
+  return updateToken.access_token;
+}
