11b014ebff1fba8f62da70288bd891145c9726c8
Daniil <daniil.shishaev@gmail.com>
2025-07-23 14:34:52 +0300

    feat: users table pagination

--- src/entities/user/ui/users-table.ui.tsx
+++ src/entities/user/ui/users-table.ui.tsx
@@ -2,8 +2,10 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import { UserRow, UsersTableHead, useSortedUsers } from '~/entities';
 import { type User } from '~/shared';
-import { useState } from 'react';
+import { type ChangeEvent, useMemo, useState } from 'react';
 import { useAuth } from '~/app';
+import React from 'react';
+import TablePagination from '@mui/material/TablePagination';
 
 type UsersTableProps = {
   users: User[];
@@ -17,27 +19,57 @@ export const UsersTable: React.FC<UsersTableProps> = (props) => {
   const [orderBy, setOrderBy] = useState<string>('');
   const [orderDirection, setOrderDirection] = useState<'asc' | 'desc'>('asc');
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage] = useState(50);
+
   const currentUser = users.find((u) => u.id == currentProfileId);
   const otherUsers = users.filter((u) => u.id !== currentProfileId);
-  const sortedUsers = useSortedUsers(otherUsers, orderBy, orderDirection);
+
+  const paginatedUsers = useMemo(() => {
+    const start = currentPage * rowsPerPage;
+    return otherUsers.slice(start, start + rowsPerPage);
+  }, [otherUsers, currentPage, rowsPerPage]);
+
+  const sortedUsers = useSortedUsers(paginatedUsers, orderBy, orderDirection);
 
   const cellHidden = 'hidden md:table-cell';
+  const itemsCount = users.length;
+
+  function handleChangePage(_: React.MouseEvent<HTMLButtonElement, MouseEvent> | null, newPage: number) {
+    setCurrentPage(newPage);
+  }
+
+  function handleChangeRowsPerPage(event: ChangeEvent<HTMLInputElement>) {
+    setRowsPerPage(Number(event?.target.value));
+    setCurrentPage(0);
+  }
 
   return (
-    <Table className="mt-2">
-      <UsersTableHead
-        cellHidden={cellHidden}
-        orderBy={orderBy}
-        setOrderBy={setOrderBy}
-        orderDirection={orderDirection}
-        setOrderDirection={setOrderDirection}
+    <>
+      <TablePagination
+        count={itemsCount}
+        page={currentPage}
+        rowsPerPage={rowsPerPage}
+        rowsPerPageOptions={[20, 50, 75, 100, itemsCount]}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+        component={'div'}
       />
-      <TableBody>
-        {currentUser && <UserRow userData={currentUser} />}
-        {sortedUsers.map((user) => (
-          <UserRow hiddenCell={cellHidden} key={user.id} userData={user} />
-        ))}
-      </TableBody>
-    </Table>
+      <Table className="mt-2">
+        <UsersTableHead
+          cellHidden={cellHidden}
+          orderBy={orderBy}
+          setOrderBy={setOrderBy}
+          orderDirection={orderDirection}
+          setOrderDirection={setOrderDirection}
+        />
+        <TableBody>
+          {currentUser && <UserRow userData={currentUser} />}
+          {sortedUsers.map((user) => (
+            <UserRow hiddenCell={cellHidden} key={user.id} userData={user} />
+          ))}
+        </TableBody>
+      </Table>
+    </>
   );
 };
