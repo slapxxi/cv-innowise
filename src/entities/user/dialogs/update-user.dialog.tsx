c4cd748bac588cf76ea522dd6a3b4a9ce6435d6c
Daniil <daniil.shishaev@gmail.com>
2025-07-28 14:23:28 +0300

    chore: refactoring before reusing

--- src/entities/user/dialogs/update-user.dialog.tsx
+++ src/entities/user/dialogs/update-user.dialog.tsx
@@ -5,12 +5,13 @@ import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import MenuItem from '@mui/material/MenuItem';
-import type { ChangeEvent } from 'react';
-import { Suspense, useState } from 'react';
+import { Suspense } from 'react';
 import { useUpdateUser, useUpdateProfile, useUserFormData } from '~/entities/user/service';
 import { useAuth } from '~/app';
 import { CircularProgress, Box } from '@mui/material';
 import { type User } from '~/shared';
+import { useUserForm } from '~/entities/user/hooks/use-user-form-handlers.ts';
+import { UserFormFields } from '~/entities/user/ui/users-table/user-form-fields';
 
 type UpdateUserDialogPropsType = {
   dialogIsOpen: boolean;
@@ -18,50 +19,24 @@ type UpdateUserDialogPropsType = {
   user: User;
 };
 
-type FormType = {
-  email?: string;
-  password?: string;
-  firstName?: string;
-  lastName?: string;
-  departmentId?: string;
-  departmentName?: string;
-  positionId?: string;
-  positionName?: string;
-  role?: string;
-};
-
-const DialogFormSuspenced = ({ user, onClose }: { user: User; onClose: () => void }) => {
+const DialogFormSuspended = ({ user, onClose }: { user: User; onClose: () => void }) => {
   const auth = useAuth();
-  const updateUserMutation = useUpdateUser();
-  const updateProfileMutation = useUpdateProfile();
-
   const { data } = useUserFormData({ accessToken: auth!.accessToken });
   const { departments, positions } = data;
 
-  const [form, setForm] = useState<FormType>(() => ({
+  const { form, setForm, handleChange } = useUserForm({
     email: user.email,
-    firstName: user.profile?.firstName ?? '',
-    lastName: user.profile?.lastName ?? '',
+    firstName: user.profile?.firstName || '',
+    lastName: user.profile?.lastName || '',
     role: user.role,
     departmentId: user.department?.id || '',
     departmentName: user.department?.name || '',
     positionId: user.position?.id || '',
     positionName: user.position?.name || '',
-  }));
+  });
 
-  const handleDepartmentChange = (e: ChangeEvent<{ value: unknown }>) => {
-    const department = departments.find((d) => d.id === e.target.value);
-    setForm((prev) => ({ ...prev, departmentId: department?.id || '', departmentName: department?.name || '' }));
-  };
-
-  const handlePositionChange = (e: ChangeEvent<{ value: unknown }>) => {
-    const position = positions.find((p) => p.id === e.target.value);
-    setForm((prev) => ({ ...prev, positionId: position?.id || '', positionName: position?.name || '' }));
-  };
-
-  const handleChange = (field: keyof FormType) => (e: ChangeEvent<HTMLInputElement>) => {
-    setForm((prev) => ({ ...prev, [field]: e.target.value }));
-  };
+  const updateUserMutation = useUpdateUser();
+  const updateProfileMutation = useUpdateProfile();
 
   const handleSubmit = () => {
     updateProfileMutation.mutate(
@@ -92,48 +67,14 @@ const DialogFormSuspenced = ({ user, onClose }: { user: User; onClose: () => voi
       <DialogContent>
         <TextField label="Email" disabled fullWidth margin="dense" value={form.email || ''} />
         <TextField label="Password" value={'***********'} disabled fullWidth margin="dense" type="password" />
-        <TextField
-          label="First Name"
-          fullWidth
-          margin="dense"
-          value={form.firstName || ''}
-          onChange={handleChange('firstName')}
+        <UserFormFields
+          form={form}
+          setForm={setForm}
+          fields={['firstName', 'lastName', 'departmentId', 'positionId']}
+          departments={departments}
+          positions={positions}
+          onChange={handleChange}
         />
-        <TextField
-          label="Last Name"
-          fullWidth
-          margin="dense"
-          value={form.lastName || ''}
-          onChange={handleChange('lastName')}
-        />
-        <TextField
-          select
-          label="Department"
-          fullWidth
-          margin="dense"
-          value={form.departmentId || ''}
-          onChange={handleDepartmentChange}
-        >
-          {departments.map((dep) => (
-            <MenuItem key={dep.id} value={dep.id}>
-              {dep.name}
-            </MenuItem>
-          ))}
-        </TextField>
-        <TextField
-          select
-          label="Position"
-          fullWidth
-          margin="dense"
-          value={form.positionId || ''}
-          onChange={handlePositionChange}
-        >
-          {positions.map((pos) => (
-            <MenuItem key={pos.id} value={pos.id}>
-              {pos.name}
-            </MenuItem>
-          ))}
-        </TextField>
         <TextField
           select
           disabled
@@ -167,7 +108,7 @@ export const UpdateUserDialog = ({ dialogIsOpen, onClose, user }: UpdateUserDial
           </Box>
         }
       >
-        <DialogFormSuspenced user={user} onClose={onClose} />
+        <DialogFormSuspended user={user} onClose={onClose} />
       </Suspense>
     </Dialog>
   );
